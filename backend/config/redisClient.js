b9aa4594985d8fdce282b5d4a14b8cf52da21c5c
Vaishnavi S M <vaishnavi.m@GRAYMATTER.COM>
2025-08-10 15:13:09 +0530

    redis config

--- backend/config/redisClient.js
+++ backend/config/redisClient.js
@@ -1,9 +1,6 @@
 const Redis = require('ioredis');
 
-const redis = new Redis(process.env.REDIS_URL, {
-  tls: {},  // Enable SSL for Redis Cloud
-});
-
+const redis = new Redis(process.env.REDIS_URL)
 redis.on('connect', () => console.log('Redis connected'));
 redis.on('error', (err) => console.error('Redis error', err));
 
